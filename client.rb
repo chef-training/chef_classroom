05081627e8584fd03d41a084d39aa1dc43f56e9e
gmiranda23 <gmiranda@opscode.com>
2015-07-24 15:14:01 -0700

    setup chef zero usage pattern

---
+++ client.rb
@@ -0,0 +1,21 @@
+log_level        :info
+log_location     STDOUT
+profiles({
+  'default' => {
+    :driver => 'aws',
+    :machine_options => {
+      :region => 'us-east-1',
+      :ssh_username => 'ec2-user',
+      :convergence_options => {
+        :ssl_verify_mode => :verify_none,
+        :chef_version => "12.2.1"
+      },
+      :bootstrap_options => {
+        :instance_type => 'm3.medium',
+        :image_id => 'ami-1ecae776',
+        :key_name => 'aws-popup-chef'
+      }
+    }
+  }
+ }
+)
