ea6d64dec1268218d1b62a86f0fab0f8175a3cc0
gmiranda23 <gmiranda@opscode.com>
2015-07-25 01:27:48 -0700

    foodcritic fix

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name 'chef_classroom'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
+maintainer 'George Miranda (<gmiranda@chef.io>)'
+maintainer_email 'gmiranda@chef.io'
+license 'MIT'
 description 'Installs/Configures chef_classroom'
 long_description 'Installs/Configures chef_classroom'
 version '0.1.0'
-depends 'chef_classroom'
+
+depends 'chef_workstation'
