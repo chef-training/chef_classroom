e0e0551bf36ae22a2e966d619a834a0c8b4cd4e0
gmiranda23 <gmiranda@opscode.com>
2015-08-03 17:42:51 -0700

    version bump for development

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'gmiranda@chef.io'
 license 'MIT'
 description 'Installs/Configures chef_classroom'
 long_description 'Installs/Configures chef_classroom'
-version '0.1.0'
+version '0.2.0'
 
 depends 'chef_workstation'
 depends 'guacamole'
