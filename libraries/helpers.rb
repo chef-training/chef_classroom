20e141330710e1c5bca34ad2cc11738bee6560fa
Franklin Webber <franklin.webber@gmail.com>
2015-10-21 13:36:49 -0500

    FIX to work with chef-provisioning-aws 1.5.1
    The arn value needs to be specified in a hash and not a string.

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -219,7 +219,8 @@ module ChefHelpers # Helper Module for general purposes
       options[:is_windows] = true
     end
     if group == 'portal'
-      options[:bootstrap_options][:iam_instance_profile] = node['chef_classroom']['iam_instance_profile']
+      # options[:bootstrap_options][:iam_instance_profile] = node['chef_classroom']['iam_instance_profile']
+      options[:bootstrap_options][:iam_instance_profile] = { :arn => node['chef_classroom']['iam_instance_profile'] }
     end
     unless group == 'portal'
       options[:use_private_ip_for_ssh] = true
