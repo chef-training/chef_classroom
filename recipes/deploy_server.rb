f9a15b09e8ac473345b56990dfcc97838c0704b6
Seth Thomas <sthomas@chef.io>
2015-11-01 14:31:22 -0800

    Add chefserver to data_bag with everything else

--- recipes/deploy_server.rb
+++ recipes/deploy_server.rb
@@ -3,14 +3,23 @@
 
 require 'chef/provisioning/aws_driver'
 with_driver "aws::#{region}"
-name = node['chef_classroom']['class_name']
+class_name = node['chef_classroom']['class_name']
 
 include_recipe 'chef_portal::_refresh_iam_creds'
 
-machine "#{name}-chefserver" do
+machine "#{class_name}-chefserver" do
   machine_options create_machine_options(region, 'marketplace', server_size, portal_key, 'chef_server')
-  tag 'chefserver'
+  tags [ 'chefserver', class_name ]
   recipe 'chef_classroom::server'
+end
+
+chef_data_bag 'class_machines'
+
+1.upto(count) do |i|
+  chef_classroom_lookup "#{class_name}-chefserver" do
+    tags [ 'chefserver', class_name ]
+    platform 'amazon'
+  end
 end
 
 include_recipe 'chef_classroom::_refresh_portal'
