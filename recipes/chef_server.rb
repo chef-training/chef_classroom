bb2bacac9f24f3feeee8fad7c108bb85b19f170b
Seth Thomas <sthomas@chef.io>
2015-11-02 16:13:46 -0800

    Trying this with heredoc tags

--- recipes/chef_server.rb
+++ recipes/chef_server.rb
@@ -4,11 +4,13 @@
 chef_server = node['chef_classroom']['chef_server']
 
 execute 'Setup Marketplace Chef Server' do
-command "chef-marketplace-ctl setup -y
-  --username #{chef_server[:admin_user]}
-  --password #{chef_server[:admin_pass]}
-  --firstname #{chef_server[:first_name]}
-  --lastname #{chef_server[:last_name]}
-  --email #{chef_server[:e_mail]}
-  --org #{chef_server[:default_org]}"
+  command <<-EOH
+  chef-marketplace-ctl setup -y \
+  --username #{chef_server['admin_user']} \
+  --password #{chef_server['admin_pass']} \
+  --firstname #{chef_server['first_name']} \
+  --lastname #{chef_server['last_name']} \
+  --email #{chef_server['e_mail']} \
+  --org #{chef_server['default_org']}
+  EOH
 end
